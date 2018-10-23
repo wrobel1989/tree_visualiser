5bb80ec56f255a6be6bf99f9a90106a289251408
Pawel Wrobel <wrobel1989@gmail.com>
2018-10-23 16:41:31 +0200

    Add some run-time description for the empty Tree test.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,14 +5,18 @@
 // gtest with headers installed
 #include <gtest/gtest.h>
 
+#define GTEST_COUT std::cerr << "[          ] [ INFO ]"
 
 //create empty tree with empty parent node and sanitize it
 TEST(SanityEmptyTreeTest, EmptyTreeIsNullAndHasCorrectName) {
     Tree empty;
+    GTEST_COUT << "Right now, I am creating the empty tree with one empty node" << std::endl;
+    GTEST_COUT << "And checking if that node has the correct type, typename and if it is "
+                  "indeed empty of any children" << std::endl;
     ASSERT_EQ(0, empty.getParentNode()->numberOfChilden);
     ASSERT_EQ(NULL, empty.getParentNode()->children);
     ASSERT_EQ(node, empty.getParentNode()->type);
-    int cmp_string_content_result1 = strcmp(empty.getParentNode()->objectName, "integer"); //returned 0 means string equal
+    int cmp_string_content_result1 = strcmp(empty.getParentNode()->objectName, "integer"); //returned 0 means strings equal
     ASSERT_NE(0, cmp_string_content_result1); //assert it is not "integer"
     int cmp_string_content_result2 = strcmp(empty.getParentNode()->objectName, "Node");
     ASSERT_EQ(0, cmp_string_content_result2); //assert it is "Node" - that is the correct name for the
